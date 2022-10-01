899982d9cae56c17f7ab4f77aaa6a479e4217573
TranTam <tamvaa1306@gmail.com>
2022-10-01 12:20:58 +0700

    css styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <div className="calculator-grid">
         <div className="output">
-          <div className="previous-operand"></div>
-          <div className="current-operand"></div>
+          <div className="previous-operand">123,456*1</div>
+          <div className="current-operand">123,456</div>
         </div>
         <button className="span-two">AC</button>
         <button>DEL</button>
